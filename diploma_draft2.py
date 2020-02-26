38434ef1bd62346272f936b8303fb5e86731addd
Olga Terpugova <olga-terp@yandex.ru>
2020-02-26 23:27:56 +0400

    d2

--- diploma_draft2.py
+++ diploma_draft2.py
@@ -1,12 +1,6 @@
-# from urllib.parse import urlencode
 import requests
 from pprint import pprint
 import time
-
-# app_id = '7334295'
-# username = 'eshmargunov'
-
-token = '73eaea320bdc0d3299faa475c196cfea1c4df9da4c6d291633f9fe8f83c08c4de2a3abf89fbc3ed8a44e1'
 
 def get_params():
     return {
@@ -15,38 +9,27 @@ def get_params():
         'v': 5.103
     }
 
-# def get_friends():
-#     params = get_params()
-#     print('*','getting friends')
-#     response = requests.get(
-#         'https://api.vk.com/method/friends.get',
-#         params
-#     )
-#     return response.json()
-
 def get_groups():
     params = get_params()
     params['extended'] = 0
-    params['count'] = 200
-    params['fields'] = ['name', 'members_count']
-    print('*', 'запрос списка групп пользователя')
+    print('*', 'запрашиваю список групп пользователя')
     response = requests.get(
         'https://api.vk.com/method/users.getSubscriptions',
         params
     )
     return response.json()
 
-members_by_group = {}
-groups_wo_friends = {}
 def get_members():
+    members_by_group = {}
+    groups_wo_friends = {}
     groups = get_groups()
     groups_list = groups['response']['groups']['items']
     for group in groups_list:
-        time.sleep(3)
+        time.sleep(0.34)
         params = get_params()
         params['group_id'] = group
         params['filter'] = 'friends'
-        print(f'* друзья в группе {group}', sep = '')
+        print(f'* ищу друзей в группе {group}')
         response = requests.get(
             'https://api.vk.com/method/groups.getMembers',
             params
@@ -64,7 +47,8 @@ def groups_info():
         params = get_params()
         params['group_id'] = group
         params['fields'] = ['name', 'id', 'members_count']
-        print(f'* информация о группе {group}', sep = '')
+        print(f'* запрашиваю информацию о группе {group}')
+        time.sleep(0.34)
         response = requests.get(
             'https://api.vk.com/method/groups.getById',
             params
@@ -74,19 +58,8 @@ def groups_info():
     return groups
 
 if __name__ == '__main__':
-    userid = input('Ведите id пользователя Вконтакте ')  # 171691064
-
-    # friends = get_friends()
-    # friends_list = friends['response']['items']
-    # groups = get_groups()
-    # groups_list = groups['response']['groups']['items']
-    # groups_wo_friends = get_members()
+    token = '73eaea320bdc0d3299faa475c196cfea1c4df9da4c6d291633f9fe8f83c08c4de2a3abf89fbc3ed8a44e1'
+    userid = input('Введите id пользователя Вконтакте ')  # 171691064 # 18079762
     groups = groups_info()
     pprint(groups)
 
-
-
-
-# pprint(friends_list)
-# pprint(groups_list)
-# pprint(groups_wo_friends)
